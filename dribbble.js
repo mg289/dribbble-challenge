23cb9da5b43649e2670a7e81db949ad5f54b178d
M M <magdamukhtar7@gmail.com>
2013-05-26 14:00:49 -0400

    fix indent

--- dribbble.js
+++ dribbble.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
   function render(shots){
     for (var i in shots){
       var image = $('<div></div>')
-				   .append($('<img></img>')
-				   .addClass("resize")
-				   .attr({src : shots[i].image_teaser_url}));
-	  var likes = $('<div></div>')
-				   .addClass("shot_info")
-				   .append(shots[i].likes_count + " Likes");
+                   .append($('<img></img>')
+                   .addClass("resize")
+                   .attr({src : shots[i].image_teaser_url}));
+      var likes = $('<div></div>')
+                   .addClass("shot_info")
+                   .append(shots[i].likes_count + " Likes");
       var comments = $('<div></div>')
-					  .addClass("shot_info")
-					  .append(shots[i].comments_count + " Comments");
+                      .addClass("shot_info")
+                      .append(shots[i].comments_count + " Comments");
       var shot = $('<li></li>')	  
-	              .append($('<div></div>')
-				         .addClass("shot")
-					     .append(image)
-					     .append(likes)
-					     .append(comments));
+                  .append($('<div></div>')
+                  .addClass("shot")
+                  .append(image)
+                  .append(likes)
+                  .append(comments));
       $('.shot_grid').append(shot);
     }
   };
@@ -27,7 +27,7 @@ $(document).ready(function() {
   function get_shots(url){
     page_num++;
     $.ajax({
-	   dataType: "jsonp"
+       dataType: "jsonp"
       ,url: url
       ,data: {page:page_num.toString(), per_page:shots_per_load}
       ,success: function(data) {
@@ -39,8 +39,8 @@ $(document).ready(function() {
 
   $('.tabs li').click(function(){
     if (!$(this).hasClass("selected")){
-	  page_num = 1;
-	  var url = "http://api.dribbble.com/shots/" + this.id;
+      page_num = 1;
+      var url = "http://api.dribbble.com/shots/" + this.id;
       $('.selected').removeClass("selected");
       $(this).addClass("selected");
       $('.shot_grid').html("");
@@ -51,7 +51,7 @@ $(document).ready(function() {
   $('#load_more').click(function(){
     var list = $('.selected').attr('id');
     var url = "http://api.dribbble.com/shots/" + list;
-	get_shots(url);
+    get_shots(url);
   });
   
   get_shots("http://api.dribbble.com/shots/popular");
